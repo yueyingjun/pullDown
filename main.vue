36edbc71477580b12548f40d8043a92ddf6c6558
yueyingjuna <1046396106@qq.com>
2017-12-05 11:52:38 +0800

    pulldown

---
+++ main.vue
@@ -0,0 +1,65 @@
+<template>
+  <div style="position: absolute;left:0;top:0;width:100%;">
+
+      <Header1></Header1>
+      <Slider></Slider>
+      <pullDown :callback="fn" :autoLoad="true">
+           <List :data="datas" :index="index"></List>
+      </pullDown>
+  </div>
+</template>
+
+<script>
+    import Header1 from "@/components/header"
+    import Slider from "@/components/slider"
+    import List from "@/components/list"
+    import pullDown from "@/components/pullDown"
+    export default {
+        name: 'hello',
+        components:{Header1,Slider,List,pullDown},
+        data () {
+            return {
+                datas:[],
+                page:0,
+                index:["title"]
+            }
+        },
+        methods:{
+           fn(obj){
+               /*用户体验*/
+               if(this.$root.routerPlace){
+                   this.datas=this.$root.datas;
+                   this.$root.page= this.$root.page?++this.$root.page:this.page;
+                   this.page=this.$root.page;
+                   return;
+               }
+               fetch("/ajax/news/select?page="+this.page).then(function(e){
+                   return e.json();
+               }).then((e)=>{
+                   setTimeout(()=>{
+                       this.datas=e.concat(this.datas);
+                       this.$root.datas=this.datas;
+                       obj.over();
+                       this.page++;
+                       this.$root.page=this.page;
+                   },2000)
+
+               })
+           }
+
+        },
+
+        destroyed(){
+            this.$root.routerPlace=true;
+        }
+
+
+
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pullDown.vue
@@ -0,0 +1,110 @@
+<template>
+     <div class="pullDownBox" @touchstart="start($event)" ref="pullDownBox">
+         <div class="notice" v-show="noticeFlag">
+             {{noticeMessage}}
+
+         </div>
+
+         <div class="pullDownCon" ref="pullDownCon">
+            <slot></slot>
+         </div>
+
+     </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        props:["callback","autoLoad","routerReplace"],
+        data () {
+            return {
+                noticeMessage:"下拉刷新....",
+                noticeFlag:false,
+                starty:0,
+                movey:0,
+                pullDownBoxEle:null,
+                pullDownConEle:null,
+                pullFlag:true
+            }
+        },
+
+        mounted(){
+
+            this.pullDownBoxEle=this.$refs["pullDownBox"];
+            this.pullDownConEle=this.$refs["pullDownCon"];
+
+          if(this.autoLoad){
+              if(this.callback){
+                  this.callback(this);
+              }
+          }
+        },
+        methods:{
+            start(e){
+
+              if(!this.pullFlag){
+                  return;
+              }
+               this.noticeMessage="下拉刷新...."
+              this.pullFlag=false
+              this.starty=e.touches[0].pageY;
+              this.pullDownBoxEle.addEventListener("touchmove",this.move);
+              document.body.addEventListener("touchend",this.end)
+            },
+
+            move(e){
+                this.noticeFlag=true;
+                this.movey=e.touches[0].pageY;
+                if(this.movey-this.starty>20){
+                    this.pullDownConEle.style.transition=null;
+                    this.$root.routerPlace=false;
+                    this.pullDownConEle.style.transform="translate(0,"+(this.movey-this.starty)/6+"px)"
+                }
+            },
+            end(e){
+                this.pullDownConEle.style.transition="transform 1s ease";
+                this.pullDownConEle.style.transform="translate(0,30px)"
+
+                this.noticeMessage="正在刷新..."
+                this.pullDownBoxEle.removeEventListener("touchmove",this.move);
+                document.body.removeEventListener("touchend",this.end);
+                if(this.callback){
+                    this.callback(this);
+                }
+            },
+
+            over(){
+                this.pullDownConEle.style.transform="translate(0,0)";
+                this.noticeMessage="加载完毕!";
+                var t=setTimeout(()=>{
+                    clearTimeout(t);
+                this.noticeFlag=false;
+                this.pullFlag=true;
+                },1000)
+            }
+        },
+        created(){
+            console.log("created");
+        },
+        destroyed(){
+            console.log("destroyed")
+        }
+    }
+</script>
+
+<style scoped>
+.pullDownBox{
+    position: relative;width:100%;height:100%;
+    background: red;
+    overflow: scroll;
+}
+.notice{
+    position: absolute;
+    left:0;top:0;
+    width:100%;height:30px;
+    text-align: center; line-height: 30px;
+ }
+ .pullDownCon{
+
+ }
+</style>
